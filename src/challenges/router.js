778f9d741f52e4587d96bb294eddffd5ec939ec3
mohammad-samara <mohammad.ab.samara@gmail.com>
2020-10-04 19:31:09 +0300

    processing

--- src/challenges/router.js
+++ src/challenges/router.js
@@ -10,7 +10,7 @@ router.get("/", challengesCtroller.listChallenges);
 router.get("/:challengeId", challengesCtroller.getChallenge);
 
 //one challenge page before saving into the user profile
-router.post("/:challengeId/test", challengesCtroller.testChallenge);
+router.post("/:challengeId/test", challengesCtroller.testChallenge);/////send the solution in the body/////
 
 router.post("/", challengesCtroller.createChallenge);
 
