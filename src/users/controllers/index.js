1af8facca6c6d1202e05aa65e53cc9a0b92e55fc
amuammer <amuammer@students.iugaza.edu.ps>
2020-10-05 22:08:07 +0300

    fix signin

--- src/users/controllers/index.js
+++ src/users/controllers/index.js
@@ -25,7 +25,7 @@ exports.singup = async (req, res, next) => {
   User.create(req.body).then(async(user) => {
     const token = await User.generateToken(user);
     res.cookie("token", token);
-    res.redirect("/sigin");
+    res.redirect("/signin");
   })
     .catch((err) => {
       res.redirect(`/signup?message=${err.message}`);
