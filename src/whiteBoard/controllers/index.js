40ce6c808299a3f4e31064673ff49e986d1fa506
mohammad-samara <mohammad.ab.samara@gmail.com>
2020-10-06 00:55:59 +0300

    fixed the bug

--- src/whiteBoard/controllers/index.js
+++ src/whiteBoard/controllers/index.js
@@ -12,20 +12,20 @@ exports.createUserWhiteBoard = async (req, res) => {
   console.log(req.body,"111111111111111111111111111111111");
   console.log(req.params.savedChallengeId,"111111111111111111111111")
     console.log("user", req.user.validUser);
-    const results = await commentModel.create(Object.assign(req.body,{ user_challenge_id:req.params.savedChallengeId}));
+    const results = await whiteBoardModel.create(Object.assign(req.body,{ user_challenge_id:req.params.savedChallengeId}));
     res.status(200).send({ data: results });//shoud not reload or change the page//go do DOM
 };
 
 /////////////////////////////////////
 //update whiteboard by whiteboard id
 exports.updateWhiteBoard = async (req, res) => {
-  const results = await commentModel.update(req.params.whiteboardId, req.body);
+  const results = await whiteBoardModel.update(req.params.whiteboardId, req.body);
   res.status(200).send({ data: results });
 };
 
 exports.deleteWhiteBoard = async (req, res) => {
   const whiteboardId = req.params.whiteboardId;
-  const results = await commentModel.delete(whiteboardId);
+  const results = await whiteBoardModel.delete(whiteboardId);
   res.status(200).send({ data: results });
 };
 
