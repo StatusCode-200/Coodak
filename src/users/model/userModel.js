2cc7d50202b354d7e3e03f01f67e12ecadb7522f
laith <mr.laith_2011@hotmail.com>
2020-10-06 13:23:39 +0300

    updatestyle

--- src/users/model/userModel.js
+++ src/users/model/userModel.js
@@ -69,8 +69,8 @@ class User {
   getById(userId) {
     return this.schema.findOne({ _id: userId });
   }
-  update(userName, record) {
-    return this.schema.findOneAndUpdate({ userName }, record, { new: true });
+  update(_id, record) {
+    return this.schema.findOneAndUpdate({ _id }, record, { new: true });
   }
 
   patch(userName, record) {
