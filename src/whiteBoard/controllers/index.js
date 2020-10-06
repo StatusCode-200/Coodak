da935d919c04d828891f5e002aa32d96c917340d
mohammad-samara <mohammad.ab.samara@gmail.com>
2020-10-06 10:09:25 +0300

    redirect to whiteboard after saving or updating

--- src/whiteBoard/controllers/index.js
+++ src/whiteBoard/controllers/index.js
@@ -13,14 +13,17 @@ exports.createUserWhiteBoard = async (req, res) => {
   console.log(req.params.savedChallengeId,"111111111111111111111111")
     console.log("user", req.user.validUser);
     const results = await whiteBoardModel.create(Object.assign(req.body,{ user_challenge_id:req.params.savedChallengeId}));
-    res.status(200).send({ data: results });//shoud not reload or change the page//go do DOM
+    //res.status(200).send({ data: results });//shoud not reload or change the page//go do DOM
+    res.redirect(`/challenges/${req.params.savedChallengeId}/whiteboard`);
+    
 };
 
 /////////////////////////////////////
 //update whiteboard by whiteboard id
 exports.updateWhiteBoard = async (req, res) => {
   const results = await whiteBoardModel.update(req.params.whiteboardId, req.body);
-  res.status(200).send({ data: results });
+  //res.status(200).send({ data: results });
+   res.redirect(`/challenges/${req.body.user_challenge_id}/whiteboard`);
 };
 
 exports.deleteWhiteBoard = async (req, res) => {
