1a84c4d981266265ed435cc8c8b6bd21906c912d
amuammer <amuammer@students.iugaza.edu.ps>
2020-11-07 19:21:21 +0200

    fix whiteboard

--- src/whiteBoard/controllers/index.js
+++ src/whiteBoard/controllers/index.js
@@ -6,7 +6,7 @@ exports.getUserWhiteBoard = async (req, res) => {
   const userChallengeId =  req.params.userChallengeId;
   const userId = req.params.userId;
   const results = await whiteBoardModel.get(userChallengeId);
-  res.render("whiteboard",{ whiteboard: results[0], savedChallengeId : userChallengeId, userId : userId });
+  res.status(200).send({ whiteboard: results[0], savedChallengeId : userChallengeId, userId : userId });
 };
 
 exports.createUserWhiteBoard = async (req, res) => {
@@ -16,18 +16,19 @@ exports.createUserWhiteBoard = async (req, res) => {
   //const results = await whiteBoardModel.create(req.body);
   //res.status(200).send({ data: results });//shoud not reload or change the page//go do DOM
   // res.redirect(`/users/${userId}/challenges/${req.params.savedChallengeId}/whiteboard`);
-  res.render("whiteboard",{ whiteboard: results, savedChallengeId : userChallengeId, userId : userId });
+  res.status(200).send({ whiteboard: results[0], savedChallengeId : userChallengeId, userId : userId });
 
 };
 
 /////////////////////////////////////
 //update whiteboard by whiteboard id
 exports.updateWhiteBoard = async (req, res) => {
+  const userChallengeId =  req.params.userChallengeId;
   const userId = req.params.userId;
   // eslint-disable-next-line
   const results = await whiteBoardModel.update(req.params.userChallengeId, req.body);
-  //res.status(200).send({ data: results });
-  res.redirect(`/users/${userId}/challenges/${req.params.userChallengeId}/whiteboard`);
+  res.status(200).send({ whiteboard: results[0], savedChallengeId : userChallengeId, userId : userId });
+  // res.redirect(`/users/${userId}/challenges/${req.params.userChallengeId}/whiteboard`);
 };
 
 exports.deleteWhiteBoard = async (req, res) => {
