6579428778c0c2294f8286b74e4b6d8917d7357b
amuammer <amuammer@students.iugaza.edu.ps>
2020-11-09 20:04:36 +0200

    add created_at to comment

--- src/challengeComment/model/commentSchema.js
+++ src/challengeComment/model/commentSchema.js
@@ -5,6 +5,12 @@ const challengeCommentSchema = mongoose.Schema({
   user_id :{type : mongoose.Schema.Types.ObjectId, ref : "users" ,required: true},
   challenge_id :{type : mongoose.Schema.Types.ObjectId, ref : "challenges" ,required: true},
   comment : {type : String ,required: true},
+  created_at: { type: Date },
+});
+
+challengeCommentSchema.pre("save", function(next) {
+  this.created_at = Date.now();
+  next();
 });
 
 module.exports = mongoose.model("challengeComment", challengeCommentSchema);
